aa9324f47bbbe9ea68058bc9485f8a1b34c0668e
Johnson Tran <johnsontothetran1@gmail.com>
2026-06-06 12:05:27 -0500

    Handling Other Types

--- lox/Scanner.java
+++ lox/Scanner.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+
+import static lox.TokenType.*;
+
 public class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
@@ -27,7 +30,139 @@ public class Scanner {
         return tokens;
     }
 
+    private void scanToken(){
+        char c = advance();
+        switch(c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            case '!': 
+                addToken(match('=') ? BANG_EQUAL:BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL:BANG);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL:BANG);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL:BANG);
+                break;
+            case '/':
+                if(match('/')){
+                    // a comment goes until the end of the line.
+                    while(peek() != '\n' && !isAtEnd()) advance();
+                }else{
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                // Ignore whitespaces
+                break;
+            case '\n':
+                line++;
+                break;
+            case '"':
+                string();
+                break;
+            default:
+                if(isDigit(c))
+                {
+                    number();
+                }else{
+                    Lox.error(line, "Unexpected character");
+                }
+                break;
+        }
+    }
+
+    private boolean isDigit(char c)
+    {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number()
+    {
+        while(isDigit(peek())) advance();
+
+        //look for a fractional part
+        if(peek() == '.' && isDigit(peekNext()))
+        {
+            advance();
+            while(isDigit(peek())) advance();
+        }
+
+        addToken(NUMBER, Double.parseDouble(source.substring(start,current)));
+    }
+
+    private char peekNext()
+    {
+        if(current+1 >= source.length()) return '\0';
+        return source.charAt(current+1);
+    }
+
+    private void string()
+    {
+        while(peek() != '"' && !isAtEnd())
+        {
+            if(peek() == '\n') line++;
+            advance();
+        }
+
+        if(isAtEnd())
+        {
+            Lox.error(line, "Unterminated string.");
+            return;
+        }
+
+        advance();
+
+        // trim the surrounding quotes
+        String value = source.substring(start + 1, current - 1);
+        addToken(STRING, value);
+    }
+
+    //Lookahead f(x)
+    private char peek()
+    {
+        if(isAtEnd()) return '\0';
+        return source.charAt(current);
+    }
+
+    private boolean match(char exptected){
+        if(isAtEnd()) return false;
+        if(source.charAt(current) != exptected) return false;
+        
+        current++;
+        return true;
+    }
+
     private boolean isAtEnd(){
         return current >= source.length();
+    }
+
+    private char advance()
+    {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type)
+    {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal)
+    {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
     }
 }
